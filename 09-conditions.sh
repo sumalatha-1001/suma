c20c9982b62c5cfb4e4ce66e4aea914504dc3123
Sumalatha K <latha022014@gmail.com>
2026-02-01 16:10:38 +0530

    changes

---
+++ 09-conditions.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+NUMBER=$1
+
+# -gt --- is greater than 
+# -lt --- is less than
+# -eq --- is equal to
+# -ne --- is not equal to
+
+if [$NUMBER -gt 20]; then
+    echo "given number is $NUMBER > 20"
