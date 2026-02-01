616b8e08b1ce281581a5dd345718424a4f001449
Sumalatha K <latha022014@gmail.com>
2026-02-01 14:17:10 +0530

    changes

---
+++ 04-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: Hello $PERSON2, how are you?"
+echo "$PERSON2:: Hi $PERSON1, I am doing good. How are you? Thank you for asking me"
+echo "$PERSON1:: I am doing good, thank you. What are you doing?"
+echo "$PERSON2:: I am learning DevOps. Whhat about you?"
+echo "$PERSON1:: Hey, I am also learning DevOps"
