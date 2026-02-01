76638eff9a800cf5543fa17da05333ce78e4a101
Sumalatha K <latha022014@gmail.com>
2026-02-01 16:39:44 +0530

    changes

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -2,13 +2,13 @@
 
 MARKS=$1
 
-if [ $marks -ge 90 ]; then
+if [ $MARKS -ge 90 ]; then
     echo "Grade A"
-elif [ $marks -ge 75 ]; then
+elif [ $MARKS -ge 75 ]; then
     echo "Grade B"
-elif [ $marks -ge 60 ]; then
+elif [ $MARKS -ge 60 ]; then
     echo "Grade C"
-elif [ $marks -le 35 ]; then
+elif [ $MARKS -le 35 ]; then
     echo "Grade C"    
 else
     echo "Grade failed"
