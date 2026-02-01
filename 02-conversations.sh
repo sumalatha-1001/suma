59a04df2d0f4c78e619ab8207d250e93887d18aa
Sumalatha K <latha022014@gmail.com>
2026-02-01 13:55:03 +0530

    changes

---
+++ 02-conversations.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Suma:: Hello Rama, how are you?"
+echo "Rama:: Hi Suma, I am doing good. How are you? Thank you for asking me"
+echo "Suma:: I am doing good, thank you. What are you doing?"
+echo "Rama:: I am learning DevOps. Whhat about you?"
+echo "Suma:: Hey, I am also learning DevOps"
