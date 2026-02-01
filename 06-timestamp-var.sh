39dc378cfc2c6bbce45f2dbf148437d24a3479db
Sumalatha K <latha022014@gmail.com>
2026-02-01 15:11:38 +0530

    changes

--- 06-timestamp-var.sh
+++ 06-timestamp-var.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-TIMESTAMP=$(date)
+START_TIME=$(date +%s)
+END_TIME=$(date +%s)
+TOTAL_TIME=$(($END_TIME-$START_TIME))
 
-echo "script executed at: $TIMESTAMP"
+echo "script executed at: $START_TIME"
+
+sleep 10
+
+echo "script executed in $TOTAL_TIME Seconds"
