e41a9f390056f0f69e7ce9edb30a515743e6c43e
Sumalatha K <latha022014@gmail.com>
2026-02-04 17:55:31 +0530

    changes

---
+++ 11-installation.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [USERID -ne 0]; then
+    echo "please execute this script with root user access"
+    exit 1    
+fi
+
+echo "installing Nginx"
+
+dnf install nginx -y
+
+if [$? -ne 0]; then
+    echo "installing Nginx...FAILURE"
+    exit 1    
+else
+    echo "installing Nginx...SUCCESS"
+fi    
