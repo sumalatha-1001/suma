3ca19a1954fa269a4e32cab6476f3dd309c66306
Sumalatha K <latha022014@gmail.com>
2026-02-04 19:32:43 +0530

    changes

--- 11-installation.sh
+++ 11-installation.sh
@@ -17,3 +17,21 @@ if [ $? -ne 0 ]; then
 else
     echo "installing Nginx...SUCCESS"
 fi    
+
+dnf install mysql -y
+
+if [ $? -ne 0 ]; then
+    echo "Installing MySQL ... FAILURE"
+    exit 1
+else
+    echo "Installing MySQL ... SUCCESS"
+fi
+
+dnf install nodejs -y
+
+if [ $? -ne 0 ]; then
+    echo "Installing nodejs ... FAILURE"
+    exit 1
+else
+    echo "Installing nodejs ... SUCCESS"
+fi
