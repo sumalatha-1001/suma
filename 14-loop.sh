e614d26e7134f0c9461d8d730cfac8dc92d760ab
Sumalatha K <latha022014@gmail.com>
2026-02-09 22:05:11 +0530

    changes

--- 14-loop.sh
+++ 14-loop.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-for i in {1..100}
+#for i in {1..100}
 do
     echo $i
 done
 
-for ((i=1; i<=10; i++))
+#for ((i=1; i<=10; i++))
 do  
     echo "Number is $i"
 done
-    
+
+for i in {1..5}
+do
+  echo "Restart attempt $i"
+done
+
+
