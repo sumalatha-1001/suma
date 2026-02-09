892ea2d0fdca9574cd648fccdd93399f4631b923
Sumalatha K <latha022014@gmail.com>
2026-02-09 18:41:36 +0530

    changes

---
+++ 13.logs.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOG_FOLDER="/var/log/shell-script"
+LOG_FILE="/var/log/shell-script/$0.log"
+
+if [ $USERID -ne 0 ]; then
+    echo "please execute this script with root user access" | tee -a $LOG_FILE
+    exit 1    
+fi
+
+mkdir -p $LOG_FOLDER
+
+# by defaulf shell will not execute, when we call it will execute
+
+validate(){
+    if [ $1 -ne 0 ]; then
+        echo "$2...FAILURE" | tee -a $LOG_FILE
+        exit 1
+    else   
+        echo "$2...SUCCESS" | tee -a $LOG_FILE
+    fi
+}
+
+dnf install nginx -y &>> $LOG_FILE
+validate $? "Installing Nginx"
+
+dnf install mysql -y &>> $LOG_FILE
+validate $? "Installing mysql"
+
+dnf install nodejs -y &>> $LOG_FILE
+validate $? "Installing nodejs"
