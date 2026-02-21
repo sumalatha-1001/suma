3b39bee095ce98a1e5179e7b3a05170dbf0d2862
Sumalatha K <latha022014@gmail.com>
2026-02-21 15:28:21 +0530

    actions

---
+++ 17-colors.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+USERID=$(id -u)
+LOG_FOLDER="/var/log/shell-script"
+LOG_FILE="/var/log/shell-script/$0.log"
+
+if [ $USERID -ne 0 ]; then
+    echo "please execute this script with root user access" | tee -a $LOG_FILE
+    exit 1    
+fi
+
+mkdir -p $LOG_FOLDER
+
+# by defaulf shell will not execute, when we call it will execute
+
+validate(){
+    if [ $1 -ne 0 ]; then
+        echo "$2...$R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    else   
+        echo "$2...$G SUCCESS $N" | tee -a $LOG_FILE
+    fi
+}
+
+for package in $@ # sudo sh 15-loop.sh nginx mysql node.js
+do
+    dnf list installed $package &>>$LOG_FILE
+    if [ $? -ne 0 ]; then
+        echo "$package not installed, installing now"
+        dnf install $package -y &>>$LOG_FILE
+        validate $? "$pakacge installation"
+    else
+        echo "$package already installed, $Y skipping $N"
+    fi
+done
