d7a9d3d39a0a8f49c9689413ee3f9e9bc102f251
Sumalatha K <latha022014@gmail.com>
2026-02-21 22:44:34 +0530

    actions

--- 20-trap.sh
+++ 20-trap.sh
@@ -18,6 +18,15 @@ fi
 
 mkdir -p $LOGS_FOLDER
 
+validate(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$2...$R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    else   
+        echo -e "$2...$G SUCCESS $N" | tee -a $LOG_FILE
+    fi
+}
+
 for package in $@ # sudo sh 14-loops.sh nginx mysql nodejs
 do
     dnf list installed $package &>>$LOGS_FILE
